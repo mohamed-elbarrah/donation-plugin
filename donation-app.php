17713d1dffaf5b5ce46e92ed53ae858bfecb96d5
mohamed-elbarrah <elbarrahsimo@gmail.com>
2026-01-20 22:03:16 -0800

    fix: refactor the hole code of zakat.

--- donation-app.php
+++ donation-app.php
@@ -3,7 +3,7 @@
  * Plugin Name: Donation App
  * Description: A trusted digital donation platform that connects donors with verified humanitarian cases and charitable projects. made with ♥ by Mohamed ElBarrah.
  * Author: Mohamed ElBarrah
- * Version: 1.1.7
+ * Version: 1.1.8
  */
 
 if (!defined('ABSPATH')) exit;
